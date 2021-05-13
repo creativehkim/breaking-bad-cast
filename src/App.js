1e93be979dde5cf750e4b314cabdf9ef2067c890
Hyunji Kim <creative.hkim@gmail.com>
2021-05-13 19:53:34 -0400

    Adding search feature

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect} from 'react';
 import axios from 'axios';
-import './App.css';
 import Header from './components/ui/Header';
+import Search from './components/ui/Search';
+import CharacterGrid from './components/characters/CharacterGrid'
+import './App.css';
 
 const App = () => {
   const [ items, setItems ] = useState([]);
   const [ isLodaing, setIsLoading ] = useState(true);
+  const [query, setquery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const result = await axios(`https://www.breakingbadapi.com/api/characters`);
-
-      console.log(result.data)
+      const result = await axios(`https://www.breakingbadapi.com/api/characters?name=${query}`);
+      setItems(result.data)
+      setIsLoading(false)
     }
-  }, [])
+
+    fetchItems()
+  }, [query])
 
 
   return (
     <div className="container">
       <Header />
+      <Search getQuery={(q) => setquery(q)} />
+      <CharacterGrid isLodaing={isLodaing} items={items} />
     </div>
   );
 }
